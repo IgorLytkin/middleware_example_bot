66c6dd79ff47bf51443125c6ceae6d132745489e
IgorLytkin <igor.lytkin.2020@ya.ru>
2024-03-15 10:37:55 +0700

    Пример 1. Внешняя миддлварь на Update

--- middlewares/outer.py
+++ middlewares/outer.py
@@ -22,12 +22,9 @@ class FirstOuterMiddleware(BaseMiddleware):
             event.__class__.__name__
         )
 
-        result = await handler(event, data)
-
-        logger.debug('Выходим из миддлвари  %s', __class__.__name__)
-
-        return result
-
+        # Вместо `result = await handler(event, data)` пишем `return` и, соответственно,
+        # код после return выполняться тоже не будет
+        return
 
 class SecondOuterMiddleware(BaseMiddleware):
 
